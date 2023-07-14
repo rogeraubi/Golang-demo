d3fe3c2d65387764145d53b8972479224851b63d
rogeraubi <zhongjunbi@hotmail.com>
2023-07-14 15:42:19 +1000

    IMP

--- channelSelect/select.go
+++ channelSelect/select.go
@@ -3,9 +3,30 @@ package main
 import (
 	"fmt"
 	"time"
+	"context"
 )
 
+const shortDuration = 1 * time.Millisecond
 func main() {
+	d := time.Now().Add(shortDuration)
+	ctx1, cancel1 := context.WithDeadline(context.Background(), d)
+	select {
+	case <-time.After(1 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx1.Done():
+		fmt.Println(ctx1.Err())
+	}
+
+	defer cancel1()
+
+		ctx, cancel := context.WithCancel(context.Background())
+	    defer cancel()
+	    for n:= range gen(ctx) {
+		fmt.Println(n)
+		if n==5 {
+			break
+		}
+	}	
 	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(2 * time.Second)
@@ -41,8 +62,27 @@ func main() {
 
 }
 func testLog(msg string) {
-	for i := 0; i < 20; i++ {
-		time.Sleep(1*time.Second)
-		fmt.Println(i,msg)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	for n:= range gen(ctx) {
+		fmt.Println(n,msg)
+		if n==10 {
+			break
+		}
 }
+}
+func gen(ctx context.Context) <-chan int {
+	dst := make(chan int) 
+	n :=1 
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return 
+			case dst <-n:
+				n++	
+			}
+		}
+	}()
+		return dst
+	}
