185d3cab65fc857379d6225e72c61bf918a6a1d0
rogeraubi <zhongjunbi@hotmail.com>
2023-07-14 11:38:39 +1000

    IMP

--- channelSelect/select.go
+++ channelSelect/select.go
@@ -1,52 +1,48 @@
-package main 
+package main
+
 import (
 	"fmt"
 	"time"
 )
 
 func main() {
-	/*c1 :=make(chan string)
-	c2 :=make(chan string)
-
-	go func ()  {
-		time.Sleep(3* time.Second)
-		c1<-"one"
-	}()
-   
-	go func ()  {
-		time.Sleep(2* time.Second)
-		c2<- "two"
-	}()
-
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1: fmt.Println("received", msg1)
-		case msg2 := <-c2: fmt.Println("received", msg2)
-		}
-		
-	}*/
-
-	c1 := make(chan string ,1) 
-	go func ()  {
-		time.Sleep(2* time.Second)
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(2 * time.Second)
 		c1 <- "result 1"
 	}()
-
 	select {
-	case res :=<-c1: fmt.Println(res)
-	case <-time.After(1 * time.Second):fmt.Println("timeout 1")
+	case res := <-c1:
+		{
+			c11 := make(chan string, 1)
+			fmt.Println(res)
+			go func() {
+				fmt.Println("this is another test channel")
+				c11 <- "done"
+			}()
+			testLog("ok-ok")
+			<-c11
+			// testLog("late-ok")
+			testLog("later-ok")
+		}
+	case <-time.After(3 * time.Second):fmt.Println("timeout 1")
 	}
-
-	c2 :=make(chan string ,1)
-
+	c2 := make(chan string, 1)
 	go func() {
-		time.Sleep( 2* time.Second)
-			c2 <- "result 2"
+		time.Sleep(2 * time.Second)
+		c2 <- "result 2"
 	}()
-
 	select {
-	case  res:= <-c2: fmt.Println(res)
-	case  <-time.After(3 * time.Second): fmt.Println("timeout 2")
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(3 * time.Second):
+		fmt.Println("timeout 2")
 	}
 
+}
+func testLog(msg string) {
+	for i := 0; i < 20; i++ {
+		time.Sleep(1*time.Second)
+		fmt.Println(i,msg)
+	}
 }
